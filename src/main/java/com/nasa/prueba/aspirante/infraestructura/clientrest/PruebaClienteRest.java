072572ccfcfd0de53a09c2193b0a7391c60b4485
MJZF00155619 <00155619@uca.edu.sv>
2023-04-30 14:51:10 -0600

    :sparkles: Feat: Adding PruebaClienteRest, PruebaInterfaz and URL In properties

--- src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
@@ -1,5 +1,24 @@
 package com.nasa.prueba.aspirante.infraestructura.clientrest;
 
-public class PruebaClienteRest {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
 
+import com.nasa.prueba.aspirante.dominio.dto.PruebaDto;
+
+public class PruebaClienteRest {
+	
+	private final RestTemplate restTemplate;
+
+    @Value("${nasa.api.url}")
+    private String nasaApiUrl;
+
+    public PruebaClienteRest(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public PruebaDto getPruebaDto() {
+        String apiUrl = nasaApiUrl + "apollo%202011";
+        return restTemplate.getForObject(apiUrl, PruebaDto.class);
+    }
+	
 }
--- src/main/java/com/nasa/prueba/aspirante/infraestructura/repository/PruebaInterfaz.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/repository/PruebaInterfaz.java
@@ -1,5 +1,9 @@
 package com.nasa.prueba.aspirante.infraestructura.repository;
 
-public interface PruebaInterfaz {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nasa.prueba.aspirante.dominio.entities.PruebaEntity;
+
+public interface PruebaInterfaz extends JpaRepository <PruebaEntity, Long>{
 
 }
