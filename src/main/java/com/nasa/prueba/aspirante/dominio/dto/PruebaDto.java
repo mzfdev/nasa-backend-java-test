dcf76135fc2e8ec5c4065b2069b098af2c69ce62
MJZF00155619 <00155619@uca.edu.sv>
2023-04-30 12:14:47 -0600

    :sparkles: Feat: Initialize project With Database Connection in properties, PruebaDto & PruebaEntity structure

--- src/main/java/com/nasa/prueba/aspirante/dominio/dto/PruebaDto.java
+++ src/main/java/com/nasa/prueba/aspirante/dominio/dto/PruebaDto.java
@@ -1,5 +1,64 @@
 package com.nasa.prueba.aspirante.dominio.dto;
 
-public class PruebaDto {
+import java.time.LocalDateTime;
 
+public class PruebaDto {
+	private String href;
+    private String center;
+    private String title;
+    private String nasa_id;
+    private LocalDateTime created_at;
+    
+    public PruebaDto() {
+		super();
+	}
+
+	public PruebaDto(String href, String center, String title, String nasa_id, LocalDateTime created_at) {
+		super();
+		this.href = href;
+		this.center = center;
+		this.title = title;
+		this.nasa_id = nasa_id;
+		this.created_at = created_at;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getCenter() {
+		return center;
+	}
+
+	public void setCenter(String center) {
+		this.center = center;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getNasa_id() {
+		return nasa_id;
+	}
+
+	public void setNasa_id(String nasa_id) {
+		this.nasa_id = nasa_id;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
 }
--- src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
+++ src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
@@ -1,5 +1,83 @@
 package com.nasa.prueba.aspirante.dominio.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "prueba")
 public class PruebaEntity {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "href")
+    private String href;
+    
+    @Column(name = "center")
+    private String center;
+    
+    @Column(name = "title")
+    private String title;
+    
+    @Column(name = "nasa_id")
+    private String nasa_id;
+
+	public PruebaEntity(Long id, String href, String center, String title, String nasa_id) {
+		super();
+		this.id = id;
+		this.href = href;
+		this.center = center;
+		this.title = title;
+		this.nasa_id = nasa_id;
+	}
+
+	public PruebaEntity() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getCenter() {
+		return center;
+	}
+
+	public void setCenter(String center) {
+		this.center = center;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getNasa_id() {
+		return nasa_id;
+	}
+
+	public void setNasa_id(String nasa_id) {
+		this.nasa_id = nasa_id;
+	}
 
 }
