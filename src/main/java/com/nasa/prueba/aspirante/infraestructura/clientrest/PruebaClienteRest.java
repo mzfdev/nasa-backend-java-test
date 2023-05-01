fb3aa6596ab5d4bdd99b827aed6a5c988ab0ed7d
MJZF00155619 <00155619@uca.edu.sv>
2023-05-01 13:23:28 -0600

    :sparkles: Feat: Using pruebaDto instead of Entity in PruebaClienteRest

--- src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
@@ -1,13 +1,9 @@
 package com.nasa.prueba.aspirante.infraestructura.clientrest;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,36 +21,44 @@ public class PruebaClienteRest {
 	@Autowired
     private PruebaRepository pruebaRepository;
 
-    public void search(String query) {
-    	 RestTemplate restTemplate = new RestTemplate();
-    	 String url = API_BASE_URL + query;
-    	 String result = restTemplate.getForObject(url, String.class);
-    	 Optional<PruebaEntity> entity = convertResponseToEntities(result);
-    	 entity.ifPresent(pruebaRepository::save);
-    }
+	public void search(String query) {
+   	 RestTemplate restTemplate = new RestTemplate();
+   	 String url = API_BASE_URL + query;
+   	 String result = restTemplate.getForObject(url, String.class);
+   	 Optional<PruebaDto> dto = convertResponseToDto(result);
+   	 dto.ifPresent(savedEntity -> {
+   	     PruebaEntity entity = mapDtoToEntity(savedEntity);
+   	     pruebaRepository.save(entity);
+   	 });
+   }
     
     private int lastIndex = 0;
     
-    private Optional<PruebaEntity> convertResponseToEntities(String response) {
-    	 try {
-    	        ObjectMapper mapper = new ObjectMapper();
-    	        JsonNode root = mapper.readTree(response);
-    	        JsonNode items = root.get("collection").get("items");
-    	        if (items.isArray() && items.size() > 0) {
-    	            if (lastIndex >= items.size()) {
-    	                lastIndex = 0;
-    	            }
-    	            JsonNode item = items.get(lastIndex++);
-    	            JsonNode data = item.get("data").get(0);
-    	            String href = item.get("href").asText();
-    	            String center = data.get("center").asText();
-    	            String title = data.get("title").asText();
-    	            String nasa_id = data.get("nasa_id").asText();
-    	            return Optional.of(new PruebaEntity(href, center, title, nasa_id));
-    	        }
-    	    } catch (Exception e) {
-    	        e.printStackTrace();
-    	    }
-    	    return Optional.empty();
-    }
+    private Optional<PruebaDto> convertResponseToDto(String response) {
+   	 try {
+   	        ObjectMapper mapper = new ObjectMapper();
+   	        JsonNode root = mapper.readTree(response);
+   	        JsonNode items = root.get("collection").get("items");
+   	        if (items.isArray() && items.size() > 0) {
+   	            if (lastIndex >= items.size()) {
+   	                lastIndex = 0;
+   	            }
+   	            JsonNode item = items.get(lastIndex++);
+   	            JsonNode data = item.get("data").get(0);
+   	            String href = item.get("href").asText();
+   	            String center = data.get("center").asText();
+   	            String title = data.get("title").asText();
+   	            String nasa_id = data.get("nasa_id").asText();
+   	            return Optional.of(new PruebaDto(href, center, title, nasa_id));
+   	        }
+   	    } catch (Exception e) {
+   	        e.printStackTrace();
+   	    }
+   	    return Optional.empty();
+   }
+   
+   private PruebaEntity mapDtoToEntity(PruebaDto dto) {
+       ModelMapper modelMapper = new ModelMapper();
+       return modelMapper.map(dto, PruebaEntity.class);
+   }
 }
