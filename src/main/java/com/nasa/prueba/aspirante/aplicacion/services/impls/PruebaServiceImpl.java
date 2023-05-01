c27d1ad7cf6fb6427be3af198ed0f67c02e27ef7
MJZF00155619 <00155619@uca.edu.sv>
2023-05-01 15:12:29 -0600

    :sparkle: Feat: Implementing Get Method in Controller

--- src/main/java/com/nasa/prueba/aspirante/aplicacion/services/impls/PruebaServiceImpl.java
+++ src/main/java/com/nasa/prueba/aspirante/aplicacion/services/impls/PruebaServiceImpl.java
@@ -1,24 +1,14 @@
 package com.nasa.prueba.aspirante.aplicacion.services.impls;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.nasa.prueba.aspirante.aplicacion.services.PruebaService;
-import com.nasa.prueba.aspirante.dominio.dto.PruebaDto;
 import com.nasa.prueba.aspirante.dominio.entities.PruebaEntity;
-import com.nasa.prueba.aspirante.infraestructura.clientrest.PruebaClienteRest;
 import com.nasa.prueba.aspirante.infraestructura.repository.PruebaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.JpaSort;
-
-
-//PruebaServiceImpl.java
 
 @Service
 public class PruebaServiceImpl implements PruebaService {
@@ -32,8 +22,7 @@ public class PruebaServiceImpl implements PruebaService {
 
 	@Override
 	public void crearPruebaEntity(PruebaEntity pruebaEntity) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 }
 
--- src/main/java/com/nasa/prueba/aspirante/infraestructura/restcontroller/PruebaRestController.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/restcontroller/PruebaRestController.java
@@ -3,15 +3,38 @@ package com.nasa.prueba.aspirante.infraestructura.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.nasa.prueba.aspirante.dominio.dto.PruebaDto;
+import com.nasa.prueba.aspirante.aplicacion.services.PruebaService;
 import com.nasa.prueba.aspirante.dominio.entities.PruebaEntity;
-import com.nasa.prueba.aspirante.infraestructura.clientrest.PruebaClienteRest;
-import com.nasa.prueba.aspirante.infraestructura.repository.PruebaRepository;
 
+@RestController
+@RequestMapping("/Pruebas")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PruebaRestController {
 	
+	@Autowired
+	private PruebaService pruebaService;
+	
+	@GetMapping("/Pruebas_view")
+	public ResponseEntity<List<PruebaEntity>> ObtenerPruebas(){
+		try {
+			List<PruebaEntity> pruebas = pruebaService.obtenerPruebaEntities();
+			return new ResponseEntity<>(
+					pruebas,
+					HttpStatus.OK
+					);
+			
+		}catch(Exception e) {
+			return new ResponseEntity<>(
+					null,
+					HttpStatus.INTERNAL_SERVER_ERROR
+					);
+		}
+	}
 }
